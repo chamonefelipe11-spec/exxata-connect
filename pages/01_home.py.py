b36f5bb82e2fe8b4665368a5444f875f1e3d5fc1
(Cha)² <chamonefelipe11@gmail.com>
2025-09-25 15:01:00 -0300

    Delete pages directory

--- pages/01_home.py.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from lib.guards import require_auth
-user = require_auth()
-st.title(Dashboard — Exxata)
-st.write(fBem-vindo(a), {user['name']}.)
-st.page_link(pages02_Projects.py, label=Ir para Projects)
